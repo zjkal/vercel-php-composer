0b957f4da40acd327ae48b86a6c8279598be0306
zjkal <javachina@foxmail.com>
2024-08-21 13:10:23 +0800

    精简代码

--- api/index.php
+++ api/index.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Carbon\Carbon;
+use zjkal\TimeHelper;
 
-printf("现在时间是： %s", Carbon::now()->tz('Asia/Shanghai')->toDateTimeString());
+printf("现在时间是： %s", TimeHelper::now());
